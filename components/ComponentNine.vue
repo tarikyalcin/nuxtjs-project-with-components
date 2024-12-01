08a8603a67d4f2903d4ef0f0238ef36f9fe4116e
Tarik Yalcin <tarikyalcinwork@gmail.com>
2024-12-01 19:58:55 +0300

    nuxtjs-project-with-components
    A sample Nuxt.js project with 10 components, showcasing basic Vue.js functionality and structure.

---
+++ components/ComponentNine.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Merhaba, ben Component 9!</h1>
+      <p>Bu sayfa Cemre'ye ait.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ComponentSeven',
+  };
+  </script>
+  
---
+++ components/ComponentThree.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Merhaba, ben Component 3!</h1>
+      <p>Bu sayfa Tarık'a ait.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ComponentSeven',
+  };
+  </script>
+  
