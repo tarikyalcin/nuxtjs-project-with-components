325da0cdb0687c66dab5c37737d8b74829077db3
Cemre HASIRCI <cemrehasirci@hotmail.com>
2024-12-02 10:42:00 +0300

    seven-taslak

--- components/ComponentSeven.vue
+++ components/ComponentSeven.vue
@@ -1,13 +1,63 @@
 <template>
-    <div>
-      <h1>Merhaba, ben Component 7!</h1>
-      <p>Bu sayfa Cemreye ait.</p>
-    </div>
-  </template>
+
+<div class="son-bloglar">
+  <div class="baslik">
+    <h3>Blog'dan Son Yazılar</h3>
+  </div>
+  <div class="box">
+    <ul>
+      <li v-for="(blog, index) in latestBlogs" :key="index">
+        <h4>{{ blog.title }}</h4>
+        <small><i>{{ blog.date }}</i></small>
+      </li>
+    </ul>
+  </div>
+</div>
   
-  <script>
+
+
+
+</template>
+
+
+<style>
+  .son-bloglar { 
+    width: 30%;
+    padding: 10px;
+  }
+
+  .box {
+    border: 1px solid #add;
+  }
+
+  h3 {
+
+  }
+
+
+
+
+
+</style>
+
+<script setup>
+// Blogları seç
+const blogCards = document.querySelectorAll(".blog-card");
+
+// Verileri al
+const blogs = Array.from(blogCards).map((card) => {
+  const title = card.querySelector("h2").innerText.trim(); // Başlık
+  const date = card.querySelector(".date").innerText.trim(); // Tarih
+  return { title, date };
+});
+
+// Son 3 blogu seç
+const latestBlogs = blogs.slice(0, 3);
+</script>
+
+<script>
   export default {
     name: 'ComponentSeven',
   };
-  </script>
+</script>
   
